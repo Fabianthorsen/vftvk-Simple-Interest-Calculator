b0022cc8d6feca0c8b4ba2c58362a47cad097665
Fabianthorsen <57477282+Fabianthorsen@users.noreply.github.com>
2021-01-10 19:36:19 +0100

    Update script.js
    Edited js file

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  var p = document.getElementById('principal').value;
+  var i = document.getElementById('interestInput').value;
+  var t = document.getElementById('selectId').value;
+
+  var interest = (p * t * i) / 100;
+  var year = parseInt(t) + 2021;
+  var result = document.getElementById('result');
+
+  result.innerHTML =
+    "If you deposit <span class='highlight'>" +
+    p +
+    "</span>, <br> at an interest rate of <span class='highlight'>" +
+    i +
+    "%</span>.<br> You will recieve an amount of <span class='highlight'>" +
+    interest +
+    "</span>,<br> in the year <span class='highlight'>" +
+    year +
+    '</span>.';
 }
+
         
